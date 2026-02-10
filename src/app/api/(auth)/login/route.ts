bc98ee9dc22d22002079e84176581e3f496a0f58
mitrakholiya <mitrakholiya31@gmail.com>
2026-02-10 15:51:37 +0530

    final final update

--- src/app/api/(auth)/login/route.ts
+++ src/app/api/(auth)/login/route.ts
@@ -1,60 +1,65 @@
-import { NextResponse } from "next/server"
-import UserModel from "@/models/user.model"
+import { NextResponse } from "next/server";
+import UserModel from "@/models/user.model";
 import bcrypt from "bcryptjs";
 import { dbConnect } from "@/db.config/dbconnection";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export async function POST(req: Request) {
-    try {
-        const body = await req.json();
-        const { email, password } = body
-        await dbConnect()
+  try {
+    const body = await req.json();
+    const { email, password } = body;
+    await dbConnect();
 
-        const user = await UserModel.findOne({ email })
-        if (!user) {
-            return NextResponse.json({
-                success: false,
-                message: "not Match any"
-            })
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password_hash)
-
-        if (!isMatch) {
-            return NextResponse.json({
-                success: false,
-                message: "Password Is not match"
-            })
-        }
-
-        const token = await jwt.sign({
-            userId: user._id,
-            email: user.email,
-            name: user.name,
-            phone: user.phone,
-        }, process.env.JWT_SECRET!, {
-            expiresIn: "1d"
-        })
-
-        const response = NextResponse.json({
-            success: true,
-            message: "Login Success Full"
-        })
-
-        response.cookies.set("token", token, {
-            httpOnly: true
-        })
-
-        return response
-
-
-    } catch (err) {
-        console.log("server Error", err);
-
-        return NextResponse.json({
-            success: false,
-            message: "Server Error"
-        })
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return NextResponse.json({
+        success: false,
+        message: "not Match any",
+      });
     }
 
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!isMatch) {
+      return NextResponse.json({
+        success: false,
+        message: "Password Is not match",
+      });
+    }
+
+    const token = await jwt.sign(
+      {
+        userId: user._id,
+        email: user.email,
+        name: user.name,
+        phone: user.phone,
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    const response = NextResponse.json({
+      success: true,
+      message: "Login Success Full",
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (err: any) {
+    console.log("server Error", err);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Server Error",
+        error: err.message,
+      },
+      { status: 500 },
+    );
+  }
 }
--- src/app/api/(auth)/register/route.ts
+++ src/app/api/(auth)/register/route.ts
@@ -1,33 +1,36 @@
-import { NextResponse } from "next/server"
-import UserModel from "@/models/user.model"
+import { NextResponse } from "next/server";
+import UserModel from "@/models/user.model";
 import bcrypt from "bcryptjs";
-import { dbConnect } from "../../../../db.config/dbconnection";
-
+import { dbConnect } from "@/db.config/dbconnection";
 
 export async function POST(req: Request) {
-    console.log("Start...");
-    try {
-        const body = await req.json();
-        const { name, email, password, phone } = body
-        await dbConnect()
+  try {
+    const body = await req.json();
+    const { name, email, password, phone } = body;
+    await dbConnect();
+    const hashedpass = await bcrypt.hash(password, 10);
 
-        console.log("inside the block");
+    const user = await UserModel.create({
+      name,
+      email,
+      password_hash: hashedpass,
+      phone,
+    });
 
-        const hashedpass = await bcrypt.hash(password, 10)
-        console.log("Pass...", hashedpass);
-        const user = await UserModel.create({ name, email, password_hash: hashedpass, phone })
+    return NextResponse.json({
+      success: true,
+      user: { email },
+    });
+  } catch (err: any) {
+    console.log("server Error", err);
 
-        return NextResponse.json({
-            success: true,
-            user: { email }
-        })
-    } catch (err) {
-        console.log("server Error", err);
-
-        return NextResponse.json({
-            success: false,
-            message: "Server Error"
-        })
-    }
-
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Server Error",
+        error: err.message,
+      },
+      { status: 500 },
+    );
+  }
 }

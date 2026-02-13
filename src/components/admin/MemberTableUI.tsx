d34164306d70c25daedc8f8df085016041a585ec
mitrakholiya <mitrakholiya31@gmail.com>
2026-02-13 19:07:42 +0530

    direct-approvel-condition

--- src/components/admin/MemberTableUI.tsx
+++ src/components/admin/MemberTableUI.tsx
@@ -736,31 +736,33 @@ const MemberTableUI: React.FC<MemberTableUIProps> = ({
                             </svg>
                           </div>
 
-                          <div
-                            className="w-8 h-8 rounded-full text-primary flex items-center justify-center cursor-pointer hover:scale-110 transition-transform "
-                            title="Approve with Part Payment"
-                            onClick={() =>
-                              handleOpenApproveDialog(
-                                row._id,
-                                row.remaining_loan > 0 || row.loan_amount > 0,
-                              )
-                            }
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={2}
-                              stroke="currentColor"
-                              className="w-5 h-5"
+                          {row.remaining_loan > 0 && (
+                            <div
+                              className="w-8 h-8 rounded-full text-primary flex items-center justify-center cursor-pointer hover:scale-110 transition-transform "
+                              title="Approve with Part Payment"
+                              onClick={() =>
+                                handleOpenApproveDialog(
+                                  row._id,
+                                  row.remaining_loan > 0 || row.loan_amount > 0,
+                                )
+                              }
                             >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 4.5v15m7.5-7.5h-15"
-                              />
-                            </svg>
-                          </div>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={2}
+                                stroke="currentColor"
+                                className="w-5 h-5"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M12 4.5v15m7.5-7.5h-15"
+                                />
+                              </svg>
+                            </div>
+                          )}
                         </>
                       )}
                       {row.status === "approved" && (
